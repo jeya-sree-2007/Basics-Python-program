7cb1199fda0244c5ba8efc489b63b40f437ac132
jeya-sree-2007 <jeyasree667@gmail.com>
2025-04-10 19:00:22 +0530

    Add files via upload

---
+++ github.py
@@ -0,0 +1,19 @@
+a=10
+b=20
+c=a+b
+print("The sum of a and b is:", c)
+
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+c=a+b
+print("The sum of a and b is:", c)
+
+phy=int(input("Enter the marks in Physics: "))
+chem=int(input("Enter the marks in Chemistry: "))
+math=int(input("Enter the marks in Mathematics: "))
+bio=int(input("Enter the marks in Biology: "))
+eng_cutoff=math+(phy+chem)/2
+print("The cutoff for engineering is:", eng_cutoff)
+med=bio+(chem+phy)/2
+print("The cutoff for medical is:", med)
+
